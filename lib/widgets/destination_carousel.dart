803e841e2e19ed9b666fca796123a3f29bc87ec0
Yeleussiz Kanat <kanatmg1@gmail.com>
2020-01-06 17:32:40 +0600

    first commit 1

---
+++ lib/widgets/destination_carousel.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class DestinationCarousel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Попуялрные заведения',
+                style: TextStyle(
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.5,
+                ),
+              ),
+              GestureDetector(
+                onTap: () => print('sfaasf'),
+                child: Text(
+                  'Все',
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.0),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
